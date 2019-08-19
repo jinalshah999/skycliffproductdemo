274acd2117dc3a0bb7f6a2d3870d321e7b1f1485
jinalshah999 <jinalshah999@gmail.com>
2019-08-19 12:50:49 +0530

    password validation

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, Validators, AbstractControl } from "@angular/forms";
+import { FormBuilder, FormControl, Validators, AbstractControl, FormGroup } from "@angular/forms";
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
@@ -13,7 +13,11 @@ invalidName:string[]=['xyz','abc'];
   ngOnInit() {
     this.customer=this.fb.group({
       firstname:new FormControl(null,[Validators.required,Validators.minLength(3),this.checkFirstName.bind(this)]),
-      lastname:new FormControl(null)
+      lastname:new FormControl(null),
+      passwordgroup:new FormGroup({
+        password:new FormControl(null,Validators.required),
+        confirm_password:new FormControl(null,Validators.required)
+      },this.matchPassword.bind(this))
     });
   }
   checkFirstName(x:AbstractControl):{[y:string]:boolean}
@@ -23,6 +27,15 @@ invalidName:string[]=['xyz','abc'];
       return{'invalidName':true};
     }
     return null;
+  }
+  matchPassword(x:AbstractControl):{[y:string]:boolean}
+  {
+    let password=x.get('password').value;
+    let cnfpassword=x.get('confirm_password').value;
+    if(password!=cnfpassword){
+      return {'passwordNotMatched':true};
+    }
+return null;
   }
   onSubmit(){
     console.log(this.customer);
