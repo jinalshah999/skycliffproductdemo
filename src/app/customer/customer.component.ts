831b0a82d51997be319b9eb48e0a0d68be0ebca8
jinalshah999 <jinalshah999@gmail.com>
2019-08-19 11:39:56 +0530

    custom validation

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl } from "@angular/forms";
+import { FormBuilder, FormControl, Validators, AbstractControl } from "@angular/forms";
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
@@ -7,13 +7,22 @@ import { FormBuilder, FormControl } from "@angular/forms";
 })
 export class CustomerComponent implements OnInit {
 customer;
+invalidName:string[]=['xyz','abc'];
   constructor(private fb:FormBuilder) { }
 
   ngOnInit() {
     this.customer=this.fb.group({
-      firstname:new FormControl(null),
+      firstname:new FormControl(null,[Validators.required,Validators.minLength(3),this.checkFirstName.bind(this)]),
       lastname:new FormControl(null)
     });
+  }
+  checkFirstName(x:AbstractControl):{[y:string]:boolean}
+  {
+    let value=x.value;
+    if(this.invalidName.indexOf(value)!==-1){
+      return{'invalidName':true};
+    }
+    return null;
   }
   onSubmit(){
     console.log(this.customer);
