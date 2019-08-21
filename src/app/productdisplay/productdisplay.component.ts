7a33aa228a3ca008bc28eec871da574d55e4f8b3
jinalshah999 <jinalshah999@gmail.com>
2019-08-21 13:01:42 +0530

    implemented subscription to routing events and spinners

--- src/app/productdisplay/productdisplay.component.ts
+++ src/app/productdisplay/productdisplay.component.ts
@@ -11,12 +11,15 @@ import { Router } from "@angular/router";
 export class ProductdisplayComponent implements OnInit {
   arr: Product[] = [];
   name: string = "watch";
+  loading:boolean=true;
   constructor(private _data: ProductdataService,private _router:Router) {}
 
   ngOnInit() {
+    this.loading=true;
     this._data.getAllProducts().subscribe(
       (data: Product[]) => {
         this.arr = data;
+        this.loading=false;
       },
       function(error) {
         alert(error);
