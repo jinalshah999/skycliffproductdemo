00e66d866496a21e6ef0be535f2f4e738ee04ed6
jinalshah999 <jinalshah999@gmail.com>
2019-08-21 18:02:03 +0530

    implemented canload

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,16 +1,17 @@
-import { Routes,RouterModule } from "@angular/router";
+import { Routes,RouterModule, PreloadAllModules } from "@angular/router";
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { UserGuardService } from './user-guard.service';
 
 const arr : Routes=[
   {path:'',component:HomeComponent},
   {path:'login',component:LoginComponent},
   {path:'customer',loadChildren:'./customer/customer.module#CustomerModule'},
   {path:'user1',loadChildren:'./users/users.module#UsersModule'},
-  {path:'product',loadChildren:'./productdisplay/product.module#ProductModule'},
+  {path:'product',canLoad:[UserGuardService],loadChildren:'./productdisplay/product.module#ProductModule'},
   {path:'pagenotfound',component:PagenotfoundComponent},
   {path:'**',redirectTo:'/pagenotfound'}
 ];
 
-export const routing=RouterModule.forRoot(arr);
+export const routing=RouterModule.forRoot(arr,{preloadingStrategy:PreloadAllModules});
--- src/app/user-guard.service.ts
+++ src/app/user-guard.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanLoad, Route } from '@angular/router';
 import { UserdataService } from './userdisplay/userdata.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserGuardService implements CanActivate {
+export class UserGuardService implements CanActivate,CanLoad {
 
   constructor(private _userdata:UserdataService,private _router:Router) { }
 
   canActivate(route:ActivatedRouteSnapshot,state:RouterStateSnapshot){
 
       return this.isUserLoggedIn(state.url);
+  }
+  canLoad(_route:Route){
+    return this.isUserLoggedIn(_route.path);
   }
   isUserLoggedIn(url:string):boolean{
 
